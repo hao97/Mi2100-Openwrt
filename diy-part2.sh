cc2ebc69fdf502cb41cb70a21ee9badf5695abdd
hao97 <40876820+hao97@users.noreply.github.com>
2021-06-14 23:08:26 +0800

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone -b packages-19.07 https://github.com/liuran001/openwrt-packages.git package/zhplug
+git clone https://github.com/monokoo/luci-app-arpbind.git package/zhplug/luci-app-arpbind
