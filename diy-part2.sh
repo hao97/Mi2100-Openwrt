e2861606eb3c6c739a24cf88894956913e1cc91d
hao97 <40876820+hao97@users.noreply.github.com>
2022-06-10 16:38:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone -b packages https://github.com/liuran001/openwrt-packages.git package/zhplug
 git clone https://github.com/monokoo/luci-app-arpbind.git package/zhplug/luci-app-arpbind
+git clone https://github.com/cokebar/luci-app-vlmcsd.git package/zhplug/luci-app-vlmcsd
